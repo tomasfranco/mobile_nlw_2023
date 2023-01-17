9173a3bd48abd7368887f2007ad8f8ef72c21233
Tomas Franco <tomas.f.carvalho@gmail.com>
2023-01-17 01:32:32 -0300

    First Commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { 
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold } from "@expo-google-fonts/inter";
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+ const [fontsLoaded] = useFonts({
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold
+  });
+
+  if(!fontsLoaded){
+   return (
+    <Loading />
+   )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Testando para ver se está rodando legal</Text>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
     </View>
   );
 }
@@ -13,8 +32,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1b1919',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    color: "#fff",
+    fontSize: 20,
+    fontFamily: 'Inter_800ExtraBold'
   },
 });
